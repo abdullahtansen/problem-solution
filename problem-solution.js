dcc7fb8755976d1dfa38e3b228d9419bb7515d60
Abdullah Al Tansen <abdullahaltansen899@gmail.com>
2022-01-28 02:11:42 +0600

    solution

---
+++ problem-solution.js
@@ -0,0 +1,26 @@
+// Problem 1:
+const a = 6;
+const b = 4;
+const output = (3*a)+(5*b);
+console.log(output);
+
+// problem 2:
+const any = 8;
+const anyNumber = 9;
+const output2 = (3*any)+(5*anyNumber);
+console.log(output);
+
+// problem-3:
+const institution = "European it"
+console.log(institution.slice(0,1));
+console.log(institution.substr(0,1));
+// problem-4:
+console.log(institution.slice(10,11));
+console.log(institution.substr(10,11));
+// problem-5:
+console.log(institution.toUpperCase().slice(9,11));
+// problem-6:
+console.log(institution.substr(2,4));
+console.log(institution.substr(2,4).length);
+// problem-8:
+console.log(institution.replace("European it","American it"))
